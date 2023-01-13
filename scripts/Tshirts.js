0cdb2733978956727f8d63dc043b4267fb0c853a
Rishav Neogi <nrishav31@gmail.com>
2023-01-13 14:46:50 +0530

    added quantity in cart page and added pesponsiveness in cart

--- scripts/Tshirts.js
+++ scripts/Tshirts.js
@@ -20,81 +20,32 @@ function renderData(data){
         let div=document.createElement("div");
         let img=document.createElement("img");
         img.src=elem.images[0];
-        let title=document.createElement("h2");
-        title.innerText=elem.title
+        let brand=document.createElement("h2");
+        brand.innerText=elem.brand
         let category=document.createElement("h3");
         category.innerText=elem.category;
-        let brand=document.createElement("p");
-        brand.innerText=elem.brand;
+        let desc=document.createElement("p");
+        desc.innerText=elem.description;
         let price=document.createElement("p");
         price.innerText=elem.price;
         let btn=document.createElement("button");
         btn.innerText="Add to cart";
         btn.addEventListener("click",()=>{
-
+            let arrydata=JSON.parse(localStorage.getItem("products"))||[];
             let obj={
                 image:elem.images[0],
                 brand:elem.brand,
                 category:elem.category,
                 price:elem.price,
-                id:Date.now(),
+                id:arrydata.length+1,
+                quantity:1
             }
-            let arrydata=JSON.parse(localStorage.getItem("products"))||[];
             arrydata.push(obj);
             localStorage.setItem("products",JSON.stringify(arrydata));
         })
-        div.append(img,title,brand,category,price,btn);
+        div.append(img,brand,category,desc,price,btn);
         container.append(div);
 
     })
 
 }
-let results=document.getElementById("search-results");
-
-document.getElementById("search-btn").addEventListener("click", function(e) {
-    e.preventDefault();
-    var searchTerm = document.getElementById("search-input").value;
-    fetch('https://dummyjson.com/products/category/'+searchTerm)
-      .then(response => response.json())
-      .then(data => {
-        var dataString = JSON.stringify(data);
-        var enc = new TextEncoder();
-        var dataArrayBuffer = enc.encode(dataString);
-        var dataUriEncoded = base64UrlEncode(dataArrayBuffer);
-        window.location = "search.html?data="+dataUriEncoded;
-      })
-      .catch(error => {
-        console.log('Error:', error);
-      });
-  });
-  function base64UrlEncode(arrayBuffer) {
-    let base64 = '';
-    const encodings = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_';
-    const bytes = new Uint8Array(arrayBuffer);
-    const byteLength = bytes.byteLength;
-    const byteRemainder = byteLength % 3;
-    const mainLength = byteLength - byteRemainder;
-    let a, b, c, d;
-    let chunk;
-    for (let i = 0; i < mainLength; i += 3) {
-      chunk = (bytes[i] << 16) | (bytes[i + 1] << 8) | bytes[i + 2];
-      a = (chunk & 16515072) >> 18;
-      b = (chunk & 258048) >> 12;
-      c = (chunk & 4032) >> 6;
-      d = chunk & 63;
-      base64 += encodings[a] + encodings[b] + encodings[c] + encodings[d];
-    }
-    if (byteRemainder === 1) {
-      chunk = bytes[mainLength];
-      a = (chunk & 252) >> 2;
-      b = (chunk & 3) << 4;
-      base64 += encodings[a] + encodings[b] + '==';
-    } if (byteRemainder === 2) {
-      chunk = (bytes[mainLength] << 8) | bytes[mainLength + 1];
-      a = (chunk & 64512) >> 10;
-      b = (chunk & 1008) >> 4;
-      c = (chunk & 15) << 2;
-      base64 += encodings[a] + encodings[b] + encodings[c] + '=';
-    }
-    return base64;
-  }
--- scripts/cart.js
+++ scripts/cart.js
@@ -1,37 +1,65 @@
-let cartData = JSON.parse(localStorage.getItem("products")) 
-let tbody = document.querySelector("#cart-items")
-
 function renderCart() {
-let container=document.getElementById("cartproduct");
-    cartData.forEach(function(item,id) {
-        let div=document.createElement("div");
-       let image=document.createElement("img");
-       image.src=item.image;
-       let brand=document.createElement("h2");
-       brand.innerText=item.brand;
-       let category=document.createElement("h3");
-       category.innerText=item.category;
-       let price=document.createElement("p");
-       price.innerText=item.price;
-       let remove=document.createElement("button");
-    remove.innerText="Remove";
+  let cartData = JSON.parse(localStorage.getItem("products"));
+  let container = document.getElementById("cartproduct");
+  container.innerHTML = null;
+  cartData.forEach(function (item, id) {
+    let div = document.createElement("div");
+    let image = document.createElement("img");
+    image.src = item.image;
+    let brand = document.createElement("h2");
+    brand.innerText = item.brand;
+    let category = document.createElement("h3");
+    category.innerText = item.category;
+    let price = document.createElement("p");
+    price.innerText = item.price;
+    let qtdiv = document.createElement("div");
+    qtdiv.setAttribute("id", "qtdiv");
+
+    let inc = document.createElement("button");
+    let dec = document.createElement("button");
+    let qty = document.createElement("p");
+    inc.innerHTML = "+";
+    inc.onclick = () => {
+      incqty(id);
+    };
+    dec.onclick = () => {
+      decqty(id);
+    };
+    dec.innerHTML = "-";
+    qty.innerText = item.quantity;
+    let remove = document.createElement("button");
+    remove.innerText = "Remove";
     remove.onclick = () => {
-        deleteFromCart(id);
-    }
-    
-       div.append(image,brand,category,price,remove);
-       container.append(div);
-       
-
-    });
+      deleteFromCart(id);
+    };
+    qtdiv.append(inc, qty, dec);
+    div.append(image, brand, category, price, qtdiv, remove);
+    container.append(div);
+  });
 }
 
-renderCart()
+renderCart();
 let deleteFromCart = (index) => {
-    // console.log(index);
-    let cart = JSON.parse(localStorage.getItem("products"));
-    cart.splice(index,1);
-    window.location.reload();
-    localStorage.setItem("products",JSON.stringify(cart));
+  // console.log(index);
+  let cart = JSON.parse(localStorage.getItem("products"));
+  cart.splice(index, 1);
+  localStorage.setItem("products", JSON.stringify(cart));
+  renderCart(cart);
+};
+const incqty = (ind) => {
+  let cart = JSON.parse(localStorage.getItem("products"));
+  cart[ind].quantity++;
+  localStorage.setItem("products", JSON.stringify(cart));
+  renderCart(cart);
+};
+
+const decqty = (ind) => {
+  let cart = JSON.parse(localStorage.getItem("products"));
+  if (cart[ind].quantity == 1) {
+    alert("You have reached the minimum quantity of this product")
+  } else {
+    cart[ind].quantity--;
+    localStorage.setItem("products", JSON.stringify(cart));
     renderCart(cart);
-}
+  }
+};
