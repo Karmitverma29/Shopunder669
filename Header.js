d13a4b22ad910beff3457b25c04001426e829bf0
chandrashekharjoshi302 <chandrashekharjoshi02@gmail.com>
2023-01-18 16:46:36 +0530

    login page

---
+++ Header.js
@@ -0,0 +1,19 @@
+function Header(){
+
+    return `<ul class="navigation">
+    <li><a href="#">Home</a></li>
+    <li><a href="#">About</a></li>
+    <li><a href="#">Books</a></li>
+    <li><a href="#">Caps</a></li>
+    <li><a href="#">Hoodies</a></li>
+    <li><a href="#">Pants</a></li>
+    <li><a href="#">Watches</a></li>
+    <li><a href="#">Shorts</a></li>
+    <li><a href="#">Tshirts</a></li>
+    <li><a href="#">Contact</a></li>
+
+  </ul>
+`
+}
+
+export default Header
