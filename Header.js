23b411991242b2d3899c2c6c91c2958570a6abc9
karmit <karmittech2004@gmail.com>
2023-01-19 16:18:50 +0530

    added all products

--- Header.js
+++ Header.js
@@ -9,13 +9,13 @@ function Header(){
                 <li><a href="./">Home</a></li>
                 <li><a href="./">Cart</a></li>
                 <li><a href="./">About</a></li>
-                <li><a href="./front-end/caps.html">Caps</a></li>
-                <li><a href="./front-end/books.html">Books</a></li>
-                <li><a href="./front-end/watches.html">Watches</a></li>
-                <li><a href="./front-end/Tshirts.html">Tshirts</a></li>
-                <li><a href="./front-end/shorts.html">Sorts</a></li>
-                <li><a href="./front-end/pants.html">Pants</a></li>
-                <li><a href="./front-end/hoodies.html">Hoodies</a></li>
+                <li><a href="./caps.html">Caps</a></li>
+                <li><a href="./books.html">Books</a></li>
+                <li><a href="./watches.html">Watches</a></li>
+                <li><a href="./Tshirts.html">Tshirts</a></li>
+                <li><a href="./shorts.html">Sorts</a></li>
+                <li><a href="./pants.html">Pants</a></li>
+                <li><a href="./hoodies.html">Hoodies</a></li>
                 <label for="check" class="close-menu"><i class="fas fa-times"></i></label>
             </span>
             <label for="check" class="open-menu"><i class="fas fa-bars"></i></label>
