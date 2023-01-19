94e7dcfe0d1c4323fd7441564ad757791772a35e
chandrashekharjoshi302 <chandrashekharjoshi02@gmail.com>
2023-01-19 15:06:13 +0530

    navbar

--- Header.js
+++ Header.js
@@ -1,18 +1,27 @@
 function Header(){
 
-    return `<ul class="navigation">
-    <li><a href="#">Home</a></li>
-    <li><a href="#">About</a></li>
-    <li><a href="#">Books</a></li>
-    <li><a href="#">Caps</a></li>
-    <li><a href="#">Hoodies</a></li>
-    <li><a href="#">Pants</a></li>
-    <li><a href="#">Watches</a></li>
-    <li><a href="#">Shorts</a></li>
-    <li><a href="#">Tshirts</a></li>
-    <li><a href="#">Contact</a></li>
-
-  </ul>
+    return ` <header>
+    <nav>
+        <ul class='nav-bar'>
+           
+            <input type='checkbox' id='check' />
+            <span class="menu">
+                <li><a href="./">Home</a></li>
+                <li><a href="./">Cart</a></li>
+                <li><a href="./">About</a></li>
+                <li><a href="./front-end/caps.html">Caps</a></li>
+                <li><a href="./front-end/books.html">Books</a></li>
+                <li><a href="./front-end/watches.html">Watches</a></li>
+                <li><a href="./front-end/Tshirts.html">Tshirts</a></li>
+                <li><a href="./front-end/shorts.html">Sorts</a></li>
+                <li><a href="./front-end/pants.html">Pants</a></li>
+                <li><a href="./front-end/hoodies.html">Hoodies</a></li>
+                <label for="check" class="close-menu"><i class="fas fa-times"></i></label>
+            </span>
+            <label for="check" class="open-menu"><i class="fas fa-bars"></i></label>
+        </ul>
+    </nav>
+    </header>
 `
 }
 
