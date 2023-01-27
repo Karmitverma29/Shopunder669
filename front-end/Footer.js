b29e5649dd34faf45603353006dd1ba81df51fe7
chandrashekharjoshi302 <chandrashekharjoshi02@gmail.com>
2023-01-27 22:01:04 +0530

    re-Check

--- front-end/Footer.js
+++ front-end/Footer.js
@@ -17,32 +17,32 @@ return `<footer  class="footer">
             <h4>GetHelp</h4>
             <ul>
                 <li><a href="#">FAQ</a></li>
-                <li><a href="#">shipping</a></li>
-                <li><a href="#">returns</a></li>
-                <li><a href="#">order status</a></li>
-                <li><a href="#">payment options</a></li>
+                <li><a href="/">shipping</a></li>
+                <li><a href="/">returns</a></li>
+                <li><a href="./allproducts.html">order status</a></li>
+                <li><a href="./cart.html">payment options</a></li>
             </ul>
         </div>
         <div class="footer-col">
             <h4>onlineShop</h4>
             <ul>
-                <li><a href="#">Books</a></li>
-                <li><a href="#">Caps</a></li>
-                <li><a href="#">Hoodies</a></li>
-                <li><a href="#">Pants</a></li>
-             <li><a href="#">Tshirts</a></li>
-             <li><a href="#">watches</a></li>
-             <li><a href="#">Shorts</a></li>
+                <li><a href="./books.html">Books</a></li>
+                <li><a href="./caps.html">Caps</a></li>
+                <li><a href="./hoodies.html">Hoodies</a></li>
+                <li><a href="./pants.html">Pants</a></li>
+             <li><a href="./Tshirts.html">Tshirts</a></li>
+             <li><a href="./watches.html">watches</a></li>
+             <li><a href="./shorts.html">Shorts</a></li>
            
             </ul>
         </div>
         <div class="footer-col">
             <h4>followUs</h4>
             <div class="social-links">
-                <a href="#"><i class="fab fa-facebook-f"></i></a>
-                <a href="#"><i class="fab fa-twitter"></i></a>
+                <a href="https://www.facebook.com/profile.php?id=100089556677210&mibextid=ZbWKwL"><i class="fab fa-facebook-f"></i></a>
+                <a href="https://chat.whatsapp.com/JauQJXYqv1JHqOTDLYO7YF"><i class="fa-brands fa-whatsapp"></i></a>
                 <a href="#"><i class="fab fa-instagram"></i></a>
-                <a href="#"><i class="fab fa-linkedin-in"></i></a>
+                <a href="https://t.me/+jgz-DmSGWMNiMzQ1"><i class="fa-brands fa-telegram"></i></a>
             </div>
         </div>
     </div>
